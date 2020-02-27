6f65a0268b177d252c7bdf083f65ac07a5c84b1b
gzw <2281401451@qq.com>
2020-02-27 02:01:38 +0000

    个人中心

---
+++ resources/views/reg/center.blade.php
@@ -0,0 +1,16 @@
+<center><font style="color:red">个人中心</font></center>
+
+<center>
+<form>
+    <table>
+        <tr>
+            <td>APPID</td>
+            <td> </td>
+        </tr>
+        <tr>
+            <td>SECRET</td>
+            <td> </td>
+        </tr>
+    </table>
+</form>
+</center>
